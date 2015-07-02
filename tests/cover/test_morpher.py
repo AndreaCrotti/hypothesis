15ee0165cce7a9a689a65930730408696b5e8782
David R. MacIver <david@drmaciver.com>
2015-07-02 08:51:56 +0100

    Additional tests for interesting edge cases.

--- tests/cover/test_morpher.py
+++ tests/cover/test_morpher.py
@@ -19,7 +19,7 @@ from __future__ import division, print_function, absolute_import, \
 
 import hypothesis.strategies as s
 from hypothesis import find
-from hypothesis.searchstrategy.morpher import MorpherStrategy
+from hypothesis.searchstrategy.morpher import MorpherStrategy, Morpher
 
 morphers = MorpherStrategy()
 intlists = s.lists(s.integers())
@@ -28,6 +28,11 @@ intlists = s.lists(s.integers())
 def test_can_simplify_through_a_morpher():
     m = find(morphers, lambda x: bool(x.become(intlists)))
     assert m.become(intlists) == [0]
+
+
+def test_can_simplify_text_through_a_morpher():
+    m = find(morphers, lambda x: bool(x.become(s.text())))
+    assert m.become(s.text()) == '0'
 
 
 def test_can_simplify_lists_of_morphers():
@@ -42,6 +47,7 @@ def test_can_simplify_lists_of_morphers():
 
 def test_can_simplify_through_two_morphers():
     m = find(morphers, lambda x: bool(x.become(morphers).become(intlists)))
+    print(m)
     assert m.become(morphers).become(intlists) == [0]
 
 
@@ -53,6 +59,35 @@ def test_a_morpher_retains_its_data_on_reserializing():
 
 def test_can_clone_morphers_into_inactive_morphers():
     m = find(
-        s.lists(morphers), lambda x: len(x) >= 2 and x[0].become(s.booleans()))
-    assert len(m) == 2
-    assert m[1].active_strategy is not None
+        s.lists(morphers),
+        lambda x: len(x) >= 2 and x[0].become(s.integers()) >= 0)
+    m_as_ints = [x.become(s.integers()) for x in m]
+    assert m_as_ints == [0, 0]
+
+
+def test_can_simplify_a_morpher_for_two_types_simultaneously():
+    m = find(
+        morphers, lambda x: x.become(s.integers()) and x.become(s.text())
+    )
+    assert m.become(s.text()) == "0"
+    assert m.become(s.integers()) == 1
+
+
+def test_can_simplify_lists_of_morphers_with_mixed_types():
+    m = find(
+        s.lists(morphers),
+        lambda xs: any(x.become(s.text()) for x in xs) and any(
+            x.become(s.integers()) for x in xs))
+    assert 1 <= len(m) <= 2
+    m_as_ints = [x.become(s.integers()) for x in m]
+    m_as_text = [x.become(s.text()) for x in m]
+    assert "0" in m_as_text
+    assert 1 in m_as_ints
+
+
+def test_a_morpher_accumulates_strategies():
+    m = Morpher(1, 1)
+    m.become(s.integers())
+    m.become(s.text())
+    assert len(m.data) == 2
+    assert m.data[0].strategy is not m.data[1].strategy
