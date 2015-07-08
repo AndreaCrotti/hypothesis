b6377446f058631700570a14d9a49eb6e5f02223
David R. MacIver <david@drmaciver.com>
2015-07-08 12:17:14 +0100

    Generate both 0.0 and -0.0

--- src/hypothesis/searchstrategy/numbers.py
+++ src/hypothesis/searchstrategy/numbers.py
@@ -646,6 +646,7 @@ class NastyFloats(SampledFromStrategy):
             self,
             elements=[
                 0.0,
+                -0.0,
                 sys.float_info.min,
                 -sys.float_info.min,
                 -sys.float_info.max,
